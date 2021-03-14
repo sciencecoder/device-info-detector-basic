111381ece46eab169d03fbc6d41067544f11f363
Adam Orr <adamo@Adams-MacBook-Pro.local>
2021-03-14 11:59:26 -0600

    Initialized

---
+++ README.md
@@ -0,0 +1,2 @@
+
+    # device-info-detector
---
+++ program.js
@@ -0,0 +1,22 @@
+//window.navigator.userAgent.indexOf("Windows NT 6.2");
+//var language = window.navigator.userLanguage || window.navigator.language
+//window.navigator.oscpu
+var express = require('express');
+//This uses Router because it lives on nodejs-demos shared server
+var app = express().Router();
+//app.listen(process.env.PORT || 3000, process.env.IP);
+//app.listen(3000)
+var obj = {};
+app.get('/api/userdata', function(req, res) {
+  
+  var systemInfo = req.headers['user-agent'];
+  var start = systemInfo.indexOf('(')+1;
+  var end = systemInfo.indexOf(')')-start;
+  var OPSystem = systemInfo.substr(start, end);
+  
+  obj.ipaddress = req.headers['x-forwarded-for'];
+  obj.language = req.headers['accept-language'].split(',')[0];
+  obj.software = OPSystem;
+
+  res.send(JSON.stringify(obj));
+});
